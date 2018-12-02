a0f756e6896bbc9ce7c9c7a8a358bed59297b830
Afanasiev Aleksandr <afanasiev.aleksandr.92@gmail.com>
2018-12-02 19:14:34 +0200

    Remove tickers code

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,10 +1,7 @@
 import Vapor
 import Crypto
 
-struct LoginRequest: Content {
-    var api: String
-    var secret: String
-}
+
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
@@ -20,34 +17,5 @@ public func routes(_ router: Router) throws {
     router.get("todos", use: todoController.index)
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
-    
-//    router.get("tickers", use: todoController.ticker())
-    
-    // Basic "Hello, world!" example
-    router.get("tickers") { req in
-        return todoController.ticker()
-    }
-    
-    router.post("balances") { req -> String in
-        
-        let a = try req.content.decode(LoginRequest.self)
-        
-        _ = a.map({ request -> Void in
-
-            guard let apiData = Data(base64Encoded: request.api, options: Data.Base64DecodingOptions(rawValue: 0)), let secretData = Data(base64Encoded: request.secret, options: Data.Base64DecodingOptions(rawValue: 0)), let apikey = String(data: apiData as Data, encoding: String.Encoding.utf8), let secret = String(data: secretData as Data, encoding: String.Encoding.utf8) else {
-                return
-            }
-            
-            
-            
-            if apikey.suffix(6) == "kepler" && secret.suffix(6) == "lukrum" {
-                
-                todoController.history(apikey: apikey.dropLast(6).description, secret: secret.dropLast(6).description)
-                
-            }
-        })
-        
-        
-        return ""
-    }
+  
 }
